26f5e68ecebae54ce089762257d984688ac7d9e4
vr10phoenix <vardhanvachana2005@gmail.com>
2026-08-27 23:45:33 +0530

    initial commit

---
+++ include/order.hpp
@@ -0,0 +1,28 @@
+#pragma once
+#include<cstdint>
+#include<cstddef>
+
+using Price = std::int64_t;
+using Quantity = std::int64_t;
+using OrderId = std::uint64_t;
+
+enum class Side : std::uint8_t {Buy , Sell};
+
+struct Order{
+    OrderId id;
+    Price price;
+    Quantity quantity;
+    std::uint64_t timestamp;
+    Order* prev{nullptr};
+    Order* next{nullptr};
+    Order* pool_next{nullptr};
+    Side side;
+
+    void reset() noexcept{
+        id = 0; price = 0;quantity = 0;
+        side = Side::Buy;timestamp = 0;
+        prev = next = nullptr;
+    }
+};
+
+static_assert(offsetof(Order , prev) % alignof(Order*) == 0);
